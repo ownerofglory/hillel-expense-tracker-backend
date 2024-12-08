f34a9a2ddd51cfcef4516187afffb8743835e9e0
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-12-08 10:00:57 +0100

    11: [FEATURE] Set up lombok support
    - Added latest lombok version mvn dependency to pom.xml files

--- hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/model/AbstractModel.java
+++ hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/model/AbstractModel.java
@@ -1,7 +1,15 @@
 package ua.ithillel.expensetracker.model;
 
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class AbstractModel {
     private Long id;
     private LocalDateTime createdAt;
