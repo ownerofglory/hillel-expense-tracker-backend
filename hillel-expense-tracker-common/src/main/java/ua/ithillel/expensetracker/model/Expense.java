75d00bdb027a7b391d5f0d3178c8f21fcda499ad
eIIka <vladis217lak10372@stud.op.edu.ua>
2024-12-10 14:16:31 +0200

    24: [FEATURE] Complete Expense and ExpenseCategory classes
    - added amount and description fields to Expense
    - added name field to ExpenseCategory
    - linked Expense to User and ExpenseCategory

--- hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/model/Expense.java
+++ hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/model/Expense.java
@@ -1,4 +1,17 @@
 package ua.ithillel.expensetracker.model;
 
-public class Expense {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense extends AbstractModel {
+    private double amount;
+    private String description;
+    private ExpenseCategory category;
+    private User user;
 }
