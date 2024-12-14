88ce5906adf4157d2f983da60299a53a9bbef910
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-12-14 14:11:14 +0100

    44: [POC] Demonstrated imgscalr-lib
    - Image transformation code in main method
    - Result in /docs

--- hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/Application.java
+++ hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/Application.java
@@ -1,6 +1,19 @@
 package ua.ithillel.expensetracker;
 
+import org.imgscalr.Scalr;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File input = new File("docs/bill_picture.jpg");
+        BufferedImage image = ImageIO.read(input);
+        BufferedImage grayImage = Scalr.apply(image, Scalr.OP_GRAYSCALE);
+
+        File output = new File("docs/gray_bill_compressed.jpg");
+        ImageIO.write(grayImage, "jpg", output);
     }
 }
