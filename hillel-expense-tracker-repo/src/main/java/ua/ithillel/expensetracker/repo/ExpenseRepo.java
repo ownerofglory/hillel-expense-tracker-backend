e5acb22a0dbf7a20806875c12d1a963e4f1fbff1
eIIka <vladis217lak10372@stud.op.edu.ua>
2024-12-15 16:35:23 +0200

    52: [FEATURE] Add ExpenseRepo interface with required methods

---
+++ hillel-expense-tracker-repo/src/main/java/ua/ithillel/expensetracker/repo/ExpenseRepo.java
@@ -0,0 +1,57 @@
+package ua.ithillel.expensetracker.repo;
+
+import ua.ithillel.expensetracker.exception.ExpenseTrackerPersistingException;
+import ua.ithillel.expensetracker.model.Expense;
+import ua.ithillel.expensetracker.model.ExpenseCategory;
+import ua.ithillel.expensetracker.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpenseRepo {
+
+    /**
+     * Finds an expense by ID
+     *
+     * @param id the ID of the expense
+     * @return an Optional containing the found expense ir empty if not found
+     * @throws ExpenseTrackerPersistingException if any persisting error occurs
+     */
+    Optional<Expense> find(Long id) throws ExpenseTrackerPersistingException;
+
+    /**
+     * Saves an expense object
+     *
+     * @param expense the expense to save
+     * @return an Optional containing the saved expense
+     * @throws ExpenseTrackerPersistingException if any persisting error occurs
+     */
+    Optional<Expense> save(Expense expense) throws ExpenseTrackerPersistingException;
+
+    /**
+     * Finds an expense by a user
+     *
+     * @param user the user whose expenses are to be retrieved
+     * @return a list of expense belonging to the user
+     * @throws ExpenseTrackerPersistingException if any persisting error occurs
+     */
+    List<Expense> findByUser(User user) throws ExpenseTrackerPersistingException;
+
+    /**
+     * Finds an expense by a category
+     *
+     * @param category the category of the expense
+     * @return a list of expenses under the specified category
+     * @throws ExpenseTrackerPersistingException if any persisting error occurs
+     */
+    List<Expense> findByCategory(ExpenseCategory category) throws ExpenseTrackerPersistingException;
+
+    /**
+     * Deletes an expense object
+     *
+     * @param expense the expense to delete
+     * @return an Optional containing the delete expense
+     * @throws ExpenseTrackerPersistingException if any persisting error occurs
+     */
+    Optional<Expense> delete(Expense expense) throws ExpenseTrackerPersistingException;
+}
