e03f2ee53ee6a0eb1ba96c254314356b0d0cf2e1
Vladyslav Zasoba <contact@ownerofglory.com>
2025-01-03 20:58:53 +0100

    Create JPA demo

--- hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/model/Expense.java
+++ hillel-expense-tracker-common/src/main/java/ua/ithillel/expensetracker/model/Expense.java
@@ -1,5 +1,6 @@
 package ua.ithillel.expensetracker.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,24 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "t_expense")
 public class Expense extends AbstractModel {
     private double amount;
     private String description;
+
+    @Transient
     private ExpenseCategory category;
+
+    @ManyToOne
     private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "t_tag_expense",
+            joinColumns = @JoinColumn(name = "expense_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+
+    )
     private Set<ExpenseTag> tags;
 }
